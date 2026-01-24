b1e3d3809fcc0dde8bd2deb374c8f1af775db2ff
YeomanYe <ming13821007683@gmail.com>
2026-01-24 19:00:53 +0800

    feat: update blog support i18n

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,7 @@
 // Server Component - fetch data on the server
 import { getPosts, getAllTags } from '@/lib/posts';
 import BlogFilterClient from './BlogFilterClient';
-import { DEFAULT_LANGUAGE } from '@/lib/i18n';
+import { DEFAULT_LANGUAGE, t } from '@/lib/i18n';
 
 const posts = await getPosts();
 const tags = await getAllTags();
@@ -9,7 +9,7 @@ const tags = await getAllTags();
 export default function Blog() {
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-8">Articles</h1>
+      <h1 className="text-4xl font-bold mb-8">{t(DEFAULT_LANGUAGE, 'blog.title')}</h1>
 
       {/* Search and Filter Section with Posts - Client Component */}
       <BlogFilterClient posts={posts} tags={tags} locale={DEFAULT_LANGUAGE} />
